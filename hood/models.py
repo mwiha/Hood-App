3094ab8b15fc5ada5c9dc9c2401070f13b17f48c
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-02-26 16:44:35 +0300

    installed tinymce

--- hood/models.py
+++ hood/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 import datetime as dt
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     name = models.CharField(max_length=80, blank=True)
-#     bio = models.TextField(max_length=254, blank=True)
-#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
-#     location = models.CharField(max_length=50, blank=True, null=True)
-#     neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
     def __str__(self):
         return f'{self.user.username} profile'
